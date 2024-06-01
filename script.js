faae35e49aa86dcf5fb69943079b168ec6daf6a3
BHUPENDRA SINGH DEORA <122671807+bhupendra272003@users.noreply.github.com>
2024-06-01 11:08:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const slides = document.querySelectorAll('.slide');
+const numberOfSlides = slides.length;
+let slideNumber = 0;
+//slider next button 
+nextBtn.onclick = () => {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        });
+        slideNumber++;
+
+        if (slideNumber > (numberOfSlides - 1)) {
+            slideNumber = 0;
+        }
+        slides[slideNumber].classList.add('active');
+    }
+    //slider previous button 
+prevBtn.onclick = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+    slideNumber--;
+
+    if (slideNumber < 0) {
+        slideNumber = numberOfSlides - 1;
+    }
+    slides[slideNumber].classList.add('active');
+}
